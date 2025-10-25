67d486dc6ea031842cca20d4a41ac0d7bb4baa25
ClausemenDeveloper <151139919+ClausemenDeveloper@users.noreply.github.com>
2025-10-25 01:29:12 +0100

    lifecycle

--- app/src/main/java/pt/ubi/pdm/lifecycle/MainActivity.java
+++ app/src/main/java/pt/ubi/pdm/lifecycle/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 /**
  *
+ *
  * MainActivity é a tela principal da aplicação.
  * Esta classe demonstra os principais estados do ciclo de vida de uma Activity do Android,
  * exibindo o estado atual em um TextView e registrando logs.
